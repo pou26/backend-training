d08dc860b2bffa6ea806d3e7b3f642a2ca8d638c
pou26 <poushaliaich1999@gmail.com>
2022-08-12 16:44:17 +0530

    mongodb commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', route);
+
+mongoose.connect("mongodb+srv://functionUp:cszjqDBOWDuVK5I9@cluster0.ge3cm6p.mongodb.net/sidd-1db?retryWrites=true&w=majority",{
+    
+})
+.then(()=>console.log("mongoDB is connected"))
+.catch(err =>console.log(err))
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
