faa31c776194d493b4fcf98ada4769c0e94bb8fa
pou26 <poushaliaich1999@gmail.com>
2022-08-18 20:42:15 +0530

    mongo session 3

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,41 +1,17 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+const bookController= require("../controllers/bookController")
 
-router.post("/createUser", UserController.createUser  )
+const authorController= require("../controllers/authorController")
 
-router.get("/getUsersData", UserController.getUsersData)
+router.post("/createAuthor",authorController.createAuthor)
+router.post("/createBook",bookController.createBook)
+router.get("/listBooks",bookController.listBooks)
+router.get("/updatebook", bookController.updatebook)
+router.get("/bookprice", bookController.bookprice)
 
-router.post("/createBook", BookController.createBook  )
 
-router.get("/getBooksData", BookController.getBooksData)
 
-router.post("/updateBooks", BookController.updateBooks)
-router.post("/deleteBooks", BookController.deleteBooks)
-
-//MOMENT JS
-const moment = require('moment');
-router.get("/dateManipulations", function (req, res) {
-    
-    // const today = moment();
-    // let x= today.add(10, "days")
-
-    // let validOrNot= moment("29-02-1991", "DD-MM-YYYY").isValid()
-    // console.log(validOrNot)
-    
-    const dateA = moment('01-01-1900', 'DD-MM-YYYY');
-    const dateB = moment('01-01-2000', 'DD-MM-YYYY');
-
-    let x= dateB.diff(dateA, "days")
-    console.log(x)
-
-    res.send({ msg: "all good"})
-})
 
 module.exports = router;
