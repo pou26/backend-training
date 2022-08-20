b30e312863694ea17327784e5562b8d758797d39
pou26 <poushaliaich1999@gmail.com>
2022-08-21 00:31:20 +0530

    reference populate

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,11 +1,37 @@
-const authorModel = require("../models/authorModel")
+const PublisherModel = require("../models/publisherModel")
+const AuthorModel = require("../models/authorModel")
 const bookModel= require("../models/bookModel")
 
-const createBook= async function (req, res) {
+
+
+ const createBook= async function (req, res) {
     let book = req.body
+    // // console.log(author_id.length,publisher_id)
+    let a=book.author_id
+    let p=book.publisher_id
+    // if (!a){
+    //     res.send("this author field is required")
+    // }
+    // else if(!p){
+    //     res.send("this publisher field is required")
+    // }
+    // const{author_id,publisher_id}=book;
+    if(!a || !p) {
+        res.send({msg:"This detail is required"})
+    }
+    let checkauthorId=await AuthorModel.findById(a)
+    if(!checkauthorId){
+        res.send({status:false,msg:"Author id is invalid"})
+    }
+    let checkpublisherId=await PublisherModel.findById(p)
+    if(!checkpublisherId){
+        res.send({status:false,msg:"Publisher id is invalid"})
+    }
+    
     let bookCreated = await bookModel.create(book)
     res.send({data: bookCreated})
 }
+
 
 const getBooksData= async function (req, res) {
     let books = await bookModel.find()
@@ -13,11 +39,32 @@ const getBooksData= async function (req, res) {
 }
 
 const getBooksWithAuthorDetails = async function (req, res) {
-    let specificBook = await bookModel.find().populate('author_id')
+    let specificBook = await bookModel.find().populate(['author_id','publisher_id'])
+    
     res.send({data: specificBook})
 
 }
 
+const updateBook= async function (req,res){ 
+    const publisher=await PublisherModel.find({name:["HarperCollins","Penguin"]}).select({_id:1})
+    let books=await bookModel.updateMany({publisher_id:publisher},{isHardCover:true},{upsert:true})
+        let author = await AuthorModel.find({ratings:{$gt:3.5} }).select({_id:1})
+        let book1=await bookModel.updateMany({author_id:author},{$inc:{price:10}})
+        let book2=await bookModel.find()
+        res.send({book2})
+    }
+
+
+// const updateBook=async function(req,res){
+//     let publisher=await bookModel.find( {$in:[{publisher_id:"62ffa859bcd0d49fc393d6df"},{publisher_id:"62ffa8d9bcd0d49fc393d6e5"}]});
+//     // let a=publisher.map(ele=ele.publisher_id);
+//     let a=await publisher.updateMany({ratings:{$gt:3.5} }
+//                 ,{isHardCover:true,$inc:{price:10}},{new:true,upsert:true})
+//     res.send(a);
+//   }
 module.exports.createBook= createBook
 module.exports.getBooksData= getBooksData
 module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+module.exports.updateBook = updateBook
+
+
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const authorController= require("../controllers/authorController")
 const bookController= require("../controllers/bookController")
+const publisherController= require("../controllers/publisherController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -10,12 +11,18 @@ router.get("/test-me", function (req, res) {
 
 router.post("/createAuthor", authorController.createAuthor  )
 
-router.get("/getAuthorsData", authorController.getAuthorsData)
+router.post("/createPublisher", publisherController.createPublisher  )
 
 router.post("/createBook", bookController.createBook  )
+
+router.get("/getAuthorsData", authorController.getAuthorsData)
+
+router.get("/getPublishersData", publisherController.getPublishersData)
 
 router.get("/getBooksData", bookController.getBooksData)
 
 router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
+
+router.put("/updateBook", bookController.updateBook)
 
 module.exports = router;
