08437190477695f63ebb1c5ea6e87d8a3daf2fad
pou26 <poushaliaich1999@gmail.com>
2022-08-25 23:53:11 +0530

    middleware -1

--- src/index.js
+++ src/index.js
@@ -8,21 +8,41 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://poushali26:0U8on2StHP5FNKo2@cluster0.jwwwcc8.mongodb.net/poushali26", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-// app.use (
-//     function (req, res, next) {
-//         console.log ("inside GLOBAL MW");
-//         next();
-//   }
-//   );
-  
+
+
 
 app.use('/', route);
+
+app.use(
+    function(req, res, next){
+        console.log("inside GLOBAL MW");
+        console.log(Date.now(),req.ip,req.path)
+        res.send({msg:done})
+    }
+);
+
+
+
+
+
+// var address = require('address');
+// var moment = require('moment')
+// app.use(
+//     function (req, res, next) {
+//         console.log("inside GLOBAL MW");
+//         const date = moment().format('MMMM Do YYYY, hh:mm:ss a');
+//         const IP = address.ip()
+//         const api = req.path
+//         console.log(date + " , " + IP + " , " + api)
+//         next();
+//     }
+// );
 
 
 app.listen(process.env.PORT || 3000, function () {
--- src/routes/route.js
+++ src/routes/route.js
@@ -13,44 +13,47 @@ router.get("/test-me", function (req, res) {
 
 
 router.post("/createBook", BookController.createBook  )
+router.post('/createUser', UserController.createUser)
+router.get('/getUsersData', UserController.getUsersData)
+router.get('/basicRoute', UserController.basicCode)
 
 
-
+module.exports = router;
 
 // router.post("/createUser", UserController.createUser  )
 // router.get("/getUsersData", UserController.getUsersData)
 
 
-const mid1= function ( req, res, next) {
-    console.log ("inside GLOBAL MW");
-    console.log("Hi I am a middleware named Mid1")
-    // logic
-    let loggedIn = false
+// const mid1= function ( req, res, next) {
+//     console.log ("inside GLOBAL MW");
+//     console.log("Hi I am a middleware named Mid1")
+//     // logic
+//     let loggedIn = false
 
-    if (loggedIn== true) { 
-        console.log( "OK LOGGED IS IS TRUE NOW")
-        next ()
-    }
-    else {
-        res.send ("Please login or register")
-    }
-}
+//     if (loggedIn== true) { 
+//         console.log( "OK LOGGED IS IS TRUE NOW")
+//         next ()
+//     }
+//     else {
+//         res.send ("Please login or register")
+//     }
+// }
 
 /*
 
 */
 
 // e.g. restricted and open-to-all API's can be handled like below now:
-router.get('/homePage', mid1, UserController.commonHandler)
-router.get('/profileDetails', mid1, UserController.commonHandler)
-router.get('/friendList', mid1, UserController.commonHandler)
-router.get('/changePassword', mid1, UserController.commonHandler)
+// router.get('/homePage', mid1, UserController.commonHandler)
+// router.get('/profileDetails', mid1, UserController.commonHandler)
+// router.get('/friendList', mid1, UserController.commonHandler)
+// router.get('/changePassword', mid1, UserController.commonHandler)
 
-router.get('/termsAndConditions',  UserController.commonHandler)
-router.get('/register',  UserController.commonHandler, function(req, res){
-    console.log('This is last console statement')
-    res.send({status: true, msg: "Am ending the cycle."})
-})
+// router.get('/termsAndConditions',  UserController.commonHandler)
+// router.get('/register',  UserController.commonHandler, function(req, res){
+//     console.log('This is last console statement')
+//     res.send({status: true, msg: "Am ending the cycle."})
+// })
 
 
 
@@ -67,4 +70,3 @@ router.get('/register',  UserController.commonHandler, function(req, res){
 
 
 
-module.exports = router;
